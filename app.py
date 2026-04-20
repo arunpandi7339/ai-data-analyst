8ca39ecb0b28b38ebcfb365b34d871c2b59c7912
arun <arunpandi9794@gmail.com>
2026-04-20 19:49:31 +0530

    new

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
